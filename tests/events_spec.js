fb43f406b7e7112d9663787471d4ee3f6800c3a4
Brian <iamamthwiz+GitHub@gmail.com>
2015-02-16 15:24:25 -0800

    add initial events tests

---
+++ tests/events_spec.js
@@ -0,0 +1,26 @@
+require("./mock_server.js");
+require("./mock_child_process.js");
+
+describe("connects to server", function () {
+    
+});
+
+describe("emits initial command event to server", function () {
+    
+});
+
+describe("emits 'terminal-error' event to server for command error", function () {
+    
+});
+
+describe("emits 'terminal-output' event to server for command output", function () {
+    
+});
+
+describe("responds to 'commandID' event from server", function () {
+    
+});
+
+describe("responds to 'error' event from server", function () {
+    
+});

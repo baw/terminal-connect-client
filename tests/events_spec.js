b9d2ca9e4c30ca1b0a91cfa3080e305307b80374
Brian <iamamthwiz+GitHub@gmail.com>
2015-02-17 23:53:05 -0800

    redo event specs

--- tests/events_spec.js
+++ tests/events_spec.js
@@ -1,26 +1,82 @@
 require("./mock_server.js");
 require("./mock_child_process.js");
 
-describe("connects to server", function () {
+describe("events", function () {
+    describe("socket", function () {
+        describe("emits command", function () {
+            it("sends command event to server", function () {
+                
+            });
+            
+            it("sends given command with args to server", function () {
+                
+            });
+        });
+        
+        describe("on commandID", function () {
+            it("responds to 'commandID' event from server", function () {
+                
+            });
+            
+            it("saves 'commandID' given by server", function () {
+                
+            });
+        });
+        
+        describe("on error", function () {
+            it("responds to 'error' event from server", function () {
+                
+            });
+            
+            it("logs error to console", function () {
+                
+            });
+            
+            it("exits the process", function () {
+                
+            });
+        });
+    });
     
-});
-
-describe("emits initial command event to server", function () {
-    
-});
-
-describe("emits 'terminal-error' event to server for command error", function () {
-    
-});
-
-describe("emits 'terminal-output' event to server for command output", function () {
-    
-});
-
-describe("responds to 'commandID' event from server", function () {
-    
-});
-
-describe("responds to 'error' event from server", function () {
-    
+    describe("child process", function () {
+        describe("on close", function () {
+            it("emits 'terminal-output' event to server", function () {
+                
+            });
+            
+            it("sends exit code to server", function () {
+                
+            });
+        });
+        
+        describe("on error", function () {
+            it("emits 'terminal-error' event to server", function () {
+                
+            });
+            
+            it("sends error text to server", function () {
+                
+            });
+        });
+        
+        describe("on stdout data", function () {
+            it("emits 'terminal-output' event to server", function () {
+                
+            });
+            
+            it("sends output text to server", function () {
+                
+            });
+        });
+        
+        describe("on stderr data", function () {
+            it("emits 'terminal-error' event to server", function () {
+                
+            });
+            
+            it("sends error text to server", function () {
+                
+            });
+        });
+    });    
 });

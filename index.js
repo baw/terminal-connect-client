54de5733e99825f19c28bf6fd17cf7798d1fbd12
Brian <iamamthwiz+GitHub@gmail.com>
2015-03-03 16:40:39 -0800

    update command options

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ var runCommand = require("./lib/run_command.js");
 var apiKeyFileLocation = "~/.tc_apikey";
 
 var opts = require("nomnom").options({ command: {
-    position: 0,
-    help: "command to run",
-    list: false
-}, args: {
-    position: 1,
-    help: "arguments to pass to the command",
-    list: true
-}, apiKey: {
-    abbr: "a",
-    help: "apiKey used to access user account (can also be stored in '" + apiKeyFileLocation + "')"
-}, verbose: {
-    abbr: "v",
-    flag: true,
-    help: "print output from command to console"
+        position: 0,
+        help: "command to run",
+        list: false
+    }, args: {
+        position: 1,
+        help: "arguments to pass to the command",
+        list: true
+    }, apiKey: {
+        abbr: "a",
+        help: "apiKey used to access user account (can also be stored in '" + apiKeyFileLocation + "')"
+    }, verbose: {
+        abbr: "v",
+        flag: true,
+        help: "print output from command to console"
     }
 }).parse();
 
